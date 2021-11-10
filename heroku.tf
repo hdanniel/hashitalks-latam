56648b7eb25d555fe943f325051ead592a6c3db7
Hector Paz <hector@able.co>
2021-11-10 03:02:11 -0500

    Update resources

--- heroku.tf
+++ heroku.tf
@@ -16,3 +16,9 @@ resource "herokux_pipeline_github_integration" "hashitalks_latam" {
   pipeline_id = heroku_pipeline.hashitalks_latam.id
   org_repo = "hdanniel/hashitalks-latam"
 }
+
+resource "heroku_pipeline_coupling" "staging" {
+  app      = heroku_app.hashitalks_latam_demo.id
+  pipeline = heroku_pipeline.hashitalks_latam.id
+  stage    = "staging"
+}
