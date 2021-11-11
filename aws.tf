593f7357ced050e92b693fb5e4be6369a69af98c
Hector Paz <hector@able.co>
2021-11-11 10:33:25 -0500

    Update aws

--- aws.tf
+++ aws.tf
@@ -8,7 +8,7 @@ resource "aws_route53_record" "hashitalks_latam_getsysadmin_com" {
   provider = aws
   name     = "hashitalks-latam.getsysadmin.com"
   type     = "CNAME"
-  zone_id  = data.aws_route53_zone.getsysadmin.id
+  zone_id  = data.aws_route53_zone.getsysadmin_com.id
   records  = [ heroku_domain.hashitalks_latam.cname ]
   ttl      = 60
 }
